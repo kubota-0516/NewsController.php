a19ddf9b65ce735d6a3d473e42c8dd086e407c26
asahi <asahi@gmail.com>
2024-08-31 02:31:28 +0000

    larevel12まで（ログインボタン確認必要）

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -22,7 +22,24 @@
 
                     <div class="collapse navbar-collapse"id="navbarSupprtedContent">
                         <ul class="navbar-nav ms-auto"></ul>
-                        <ul class="navbar-nav"></ul>
+                        <ul class="navbar-nav">
+                          <!-- Authentication Links -->
+                            @guest
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        {{ Auth::user()->name}} <span class="caret"></span>
+                                    </a>
+                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}" onclink="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                                {{ __('messages.logout') }}
+                                            </a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </from>
+                                        </div>
+                                </li>
+                            @endguest
+                        </ul>
                     </div>
                 </div>
             </nav>
@@ -32,8 +49,12 @@
     </body>
 </html>
 
-1.Bladeテンプレートで、埋め込みたい箇所に利用するワードは何だったでしょうか？
+<!-- 1.Bladeテンプレートで、埋め込みたい箇所に利用するワードは何だったでしょうか？
 コンテンツ
 
 2.Webpackで使われているBootstrapやSCSSはどういったものか、調べられる範囲で調べてみましょう
 bootstrapは、WEB画面を作るときに便利なCSSフレームワークの一つで携帯でアクセスしたときに画面サイズが調整されるなど見やすくなる
+SCSSは、CSSをより効率的に記述できる言語、Webサイトや文書などの見た目の表示形式を定義するスタイルシートの拡張構文を提供するプリプロセッサ
+→プリプロセッサ…中心的な処理を行うプログラムに対して、その前処理（preprocess）を行うプログラムのこと。
+
+3. -->
--- routes/web.php
+++ routes/web.php
@@ -43,28 +43,3 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
     Route::get('news/create', 'add')->middleware('auth');
 });
-// ＝＝＝続き＝＝＝
-// $ cd ~/environment/mynews
-// $ php artisan migrate
-// 上記のコマンドで、ControllerやView、Routingの設定まで自動でおこなってくれます。
-// の結果↓
-// ec2-user:~/environment/mynews (main) $ php artisan migrate
-
-//    Illuminate\Database\QueryException 
-
-//   SQLSTATE[HY000] [1045] Access denied for user 'techboost'@'localhost' (using password: YES) (SQL: select * from information_schema.tables where table_schema = mynews and table_name = migrations and table_type = 'BASE TABLE')
-
-//   at vendor/laravel/framework/src/Illuminate/Database/Connection.php:760
-//     756▕         // If an exception occurs when attempting to run a query, we'll format the error
-//     757▕         // message to include the bindings with SQL, which will make this exception a
-//     758▕         // lot more helpful to the developer instead of just the database's errors.
-//     759▕         catch (Exception $e) {
-//   ➜ 760▕             throw new QueryException(
-//     761▕                 $query, $this->prepareBindings($bindings), $e
-//     762▕             );
-//     763▕         }
-//     764▕     }
-
-//       +36 vendor frames 
-//   37  artisan:35
-//       Illuminate\Foundation\Console\Kernel::handle(Object(Symfony\Component\Console\Input\ArgvInput), Object(Symfony\Component\Console\Output\ConsoleOutput))
