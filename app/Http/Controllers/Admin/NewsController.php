584237e93958f8c63dad0425b3de10cbc6eab88a
asahi <asahi@gmail.com>
2024-09-06 03:00:31 +0000

    laravel11の課題再提出

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 use App\Models\News;
 
 class NewsController extends Controller
@@ -43,6 +44,18 @@ class NewsController extends Controller
         return redirect('admin/news/create');
     }
     
+    public function index (Reqest $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+             // 検索されたら検索結果を取得する
+            $posts = News::where('title', $cond_title)->get();} else {
+                 // それ以外はすべてのニュースを取得する
+                $posts = News::all();
+            }
+            return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+
     //ここから下どこで出てくる？
     public function edit()
     {
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,16 @@
-{{-- layouts/admin.blade.phpを読み込む --}}
-@extends('layouts.admin')
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-{{-- admin.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
+{{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
 @section('title', 'プロフィールの新規作成')
 
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class='container'>
         <div class='row'>
             <div class='col-md-8 mx-auto'>
                 <h2>プロフィール新規作成</h2>
-                <form action="{{ route('admin.news.create')}}" method="post" enctype="multipart/form-data">
+                <form action="{{ route('profile.news.create')}}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
                         <ul>
