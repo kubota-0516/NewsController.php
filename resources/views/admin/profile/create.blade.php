81cec92cc5dd99283fc808426650d5dcec5d554a
asahi <asahi@gmail.com>
2024-09-10 07:13:36 +0000

    laravelAWSへのデプロイ前

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -10,7 +10,7 @@
         <div class='row'>
             <div class='col-md-8 mx-auto'>
                 <h2>プロフィール新規作成</h2>
-                <form action="{{ route('profile.blade.php)}}" method="post" enctype="multipart/form-data">
+                <form action="{{ route('profile.news.create') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
                         <ul>
