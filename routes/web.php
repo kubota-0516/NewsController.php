17deb84b62fd0ddd7811787dd30aa281f4ba7414
asahi <asahi@gmail.com>
2024-09-10 09:23:30 +0000

    laravel11課題再提出2

--- routes/web.php
+++ routes/web.php
@@ -17,27 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
-});
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+Route::controller(ProfileController::class)->prefix('admin')->middleware('auth')->group(function() {
     Route::get('profile/create', 'add');
     Route::get('profile/edit', 'edit');
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
